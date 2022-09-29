f7e906de005f47b2c21cb63b0d7a6308fe40bbcc
thn22 <107218288+thn22@users.noreply.github.com>
2022-09-29 17:38:18 -0500

    adding chart code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,13 +1,52 @@
-d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
-    console.log("data", data);
-    console.log("names", data["names"]);
-    console.log("metadata", data["metadata"]);
-    console.log("samples", data["samples"])
-    console.log("metadata[0]", data['metadata'][0]);
-    console.log("samples[0]", data['samples'][0]);
-    console.log('samples[0]["otu_ids"]', data['samples'][0]["otu_ids"]);
-    console.log('samples[0]["otu_labels"]', data['samples'][0]["otu_labels"]);
-    console.log('samples[0]["sample_values"]', data['samples'][0]["sample_values"]);
-    console.log('samples[0]["sample_values"] first 10?', data['samples'][0]["sample_values"].slice(0,10));
-});
+    d3.json(url).then(data => {
+        console.log(data);
+        let ids = data.samples[0].otu_ids;
+        console.log(ids);
+        let sampleVals = data.samples[0].sample_values.slice(0, 10).reverse();
+        console.log(sampleVals);
+        let labels = data.samples[0].otu_labels.slice(0, 10);
+        console.log(labels);
+
+        let otuTop = (data.samples[0].otu_ids.slice(0, 10)).reverse();
+        console.log(otuTop);
+
+        let otuID = otuTop.map(i => "OTU " + i);
+        console.log(`OTU IDs: ${otuID}`);
+
+
+        let trace1 = {
+            x: sampleVals,
+            y: otuID,
+            text: labels,
+            orientation: 'h',
+            type: 'bar'
+        };
+
+        let traceData = [trace1];
+
+        let layout = {
+            title: "Top 10 OTUs Found"
+        };
+        Plotly.newPlot("bar", traceData, layout);
+
+        let trace2 = {
+            x: data.samples[0].otu_ids,
+            y: data.samples[0].sample_values,
+            mode: 'markers',
+            marker: {
+                size: data.samples[0].sample_values,
+                color: data.samples[0].otu_ids
+            },
+            text: data.samples[0].otu_labels
+        };
+
+        let traceData2 = [trace2];
+
+        let layout2 = {
+            xaxis: {title: "OTU IDs"},
+        };
+
+        Plotly.newPlot('bubble', traceData2, layout2);
+    });
